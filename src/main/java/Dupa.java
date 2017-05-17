570467c49cef6e9e6a620639f33a98c741d8e2fc
Kamil Zemczak <kamilzemb@gmail.com>
2017-05-17 18:20:31 +0200

    Pierwszy commit

---
+++ src/main/java/Dupa.java
@@ -0,0 +1,18 @@
+
+/**
+ * ***********************************************************
+ * Autorskie Prawa Majątkowe Kamil Zemczak
+ *
+ * Copyright 2017 Kamil Zemczak
+ * ************************************************************
+ * Utworzono 17-05-2017, Kamil Zemczak
+ * ************************************************************
+ */
+
+/**
+ *
+ * @author Kamil Zemczak
+ */
+public class Dupa {
+    
+}
